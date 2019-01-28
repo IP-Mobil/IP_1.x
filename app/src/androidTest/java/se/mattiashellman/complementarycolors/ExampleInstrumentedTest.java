f359c7c9464e27486a2c11e9194ffb20eac04691
mhell <mattias.hellman@gmail.com>
2019-01-28 17:45:19 +0100

    Publishable

--- app/src/androidTest/java/se/mattiashellman/complementarycolors/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/se/mattiashellman/complementarycolors/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.mattiashellman.comp_colors;
+package se.mattiashellman.complementarycolors;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.mattiashellman.comp_colors", appContext.getPackageName());
+        assertEquals("se.mattiashellman.complementarycolors", appContext.getPackageName());
     }
 }
